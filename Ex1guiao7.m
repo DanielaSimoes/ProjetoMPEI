47e272668a7924499026d8535d9a4bcd21f5f527
Cristiana Carvalho <cristiana96carvalho@hotmail.com>
2015-12-12 20:01:41 +0000

    sa

--- Ex1guiao7.m
+++ Ex1guiao7.m
@@ -50,14 +50,11 @@ SimilarUsers
 
 [SimilarUsersMinHash, JD] = jaccardDistanceMinHashInt(Nu, 1000, Set, users);
 
-JD
-
-%Gr√°fico de erro
+%Gr·fico de erro
 erro= (J-JD);
 erro1 = erro(erro~=0);
 
 hist(erro1, length(erro1))
 
-hist(erro1, length(erro1))
 Var = mean(erro1.^2)- (mean(erro1))^2
 
