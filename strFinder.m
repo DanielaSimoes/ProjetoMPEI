85a0b76fbc320db32089534263f5b28dfc964e2c
Daniela Simões <danielasimoes@ua.pt>
2015-12-13 14:25:23 +0000

    Gráfico falsos positivos

--- strFinder.m
+++ strFinder.m
@@ -1,22 +1,11 @@
 
 function X = strFinder(cellArray, str)
-   
-  result = 0;
-  for n1=1:length(cellArray)
-      for n2=1:length(cellArray{n1})
-        result = strfind(cellArray{n1}{n2},str);
-
-%function X = strFinder(str, cellArray)
-%  result = 0;
-%  for n1=1:length(cellArray)
-%      for n2=1:length(cellArray)
-%        result = strmatch(char(str), char(cellArray), 'exact');
-%      end
-%  end
-  delete(h)
-  X = 1;
   
-  if cellfun(@isempty,result)
+   result = 0;
+   result = strmatch(char(str), char(cellArray), 'exact');
+    X = 1;
+  
+  if isempty((result))
     X = 0;
   end
   
